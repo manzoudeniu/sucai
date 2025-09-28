d44d3e695ffd276fd4daef2e4ebd0f2258898c79
manzoudeniu <114223078+manzoudeniu@users.noreply.github.com>
2025-09-28 12:26:05 +0800

    Create request_close.php

---
+++ request_close.php
@@ -0,0 +1,3 @@
+ {"reqNo":"DC140CD761BF6B4648258CF50027904E","closeType":"ACL","closeReason":"超过最晚审查时间未选择保持","ACL":[{"Directory":"XXXX\/1266","username":"A.L. Chen\/TAIWAN\/MXIC","FTPAccount":"alchen"}]}
+成功返回
+{"reqNo":"DC140CD761BF6B4648258CF50027904E","closeType":"ACL","closeReason":"超过最晚审查时间未选择保持","ACL":[{"Directory":"XXXX\/1266","username":"A.L. Chen\/TAIWAN\/MXIC","FTPAccount":"alchen"，"closeflag":"Y","closetime":"2025\/09\/23 23:00:00"}]}
